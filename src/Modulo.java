0849b5a372e9110afcaa53c7f20f3e445cc3e673
MiroFerko <miroslav.ferko1@gmail.com>
2022-08-13 20:02:06 +0200

    Initial commit

---
+++ src/Modulo.java
@@ -0,0 +1,24 @@
+public class Modulo {
+    //task - find all prime numbers using modulo operator
+    public static void main(String[] args) {
+        //loop for all numbers up to 50
+        for (int i = 1; i <= 50; i++) {
+
+            boolean divisorFound = false;
+            //test if i is prime
+            for (int j = 2; j <= i / 2; j++) {
+
+                //check for division remainder
+                if (i % j == 0) {
+                    divisorFound = true;
+                    break;
+                }
+            }
+
+            //if theres no more divisor print result
+            if (!divisorFound) {
+                System.out.println("Found prime number : " + i);
+            }
+        }
+    }
+}
